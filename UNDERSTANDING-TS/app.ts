875b8dd99be9309c0c5a4bf81ae899c85d98e6fc
dkagh012 <qowjdxo00@gmail.com>
2023-05-19 19:35:55 +0900

    함수 타입 및 콜백

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -2,10 +2,27 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-// void 를 명시적으로 지정 할 수 있지만 타입 스크립트는 이 코드를 추론 할 수 있는 차이가 있다
-
 function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
+
+// let someValue: undefined;
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
