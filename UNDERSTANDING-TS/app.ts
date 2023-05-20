22bd21576d49ccc59a279c33b6efe2550e555c6c
dkagh012 <qowjdxo00@gmail.com>
2023-05-20 09:49:50 +0900

    절대 타입 및 알 수 없는 타입 unknown

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,28 +1,15 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+  userName = userInput;
+}
+// never 아무것도 반환하지 않는다는것을 확실히 하기 위해 명시적으로 실행
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  // while (true) {}
 }
 
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-
-console.log(combineValues(8, 8));
-
-// let someValue: undefined;
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+generateError("An error occurred!", 500);
