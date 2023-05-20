2d171ef27541d16716d4997bd00809a55929ef52
dkagh012 <qowjdxo00@gmail.com>
2023-05-20 10:06:26 +0900

    두번쨰 강의 시작

--- UNDERSTANDING-TS/funcions.ts
+++ UNDERSTANDING-TS/funcions.ts
@@ -1,8 +1,8 @@
-function add(n1: number, n2: number) {
+function addTypeTest(n1: number, n2: number) {
   return n1 + n2;
 }
 
-function printResult(num: number): void {
+function printResultTest(num: number): void {
   console.log("Result: " + num);
 }
 
@@ -11,11 +11,11 @@ function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
   cb(result);
 }
 
-printResult(add(5, 12));
+printResultTest(addTypeTest(5, 12));
 
 let combineValues: (a: number, b: number) => number;
 
-combineValues = add;
+combineValues = addTypeTest;
 // combineValues = printResult;
 // combineValues = 5;
 
